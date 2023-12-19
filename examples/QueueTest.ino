c618be151b3c9e51cfd9ce27a5be8608f9554f6c
Rich Morgan <ionux@users.noreply.github.com>
2023-12-18 21:10:11 -0600

    stub of example sketch

--- examples/QueueTest.ino
+++ examples/QueueTest.ino
@@ -1,1 +1,17 @@
+/*
+  Arduino Queue Example Sketch
 
+  Author:  Rich Morgan <rich.l.morgan@gmail.com>
+  Date:    18/12/2023
+  Version: 1.0.0
+*/
+
+void setup()
+{
+  // TODO
+}
+
+void loop()
+{
+  // TODO
+}
